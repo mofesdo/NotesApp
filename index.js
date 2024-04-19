6266a5924a832e5852c9207c4886f2830bd8f6f5
Modesto Amador <mofesdo.dev@gmail.com>
2024-04-18 22:15:10 -0700

    trying to load notes from local storage

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 //document.querySelector("#addBtn").addEventListener("click", add);
+load();
+let notes = [
+    {num: 1, note: "Hi this is my first note"}
+]
 
+//localStorage.setItem("notes", JSON.stringify(notes))
+function load(){
+    console.log("loading notes...")
+    
+}
 
 function add(){
     console.log("adding note...")
+
 }
