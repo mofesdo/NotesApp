888fcd2c1c43a060a2162d5a6b308018be7db82d
Modesto Amador <mofesdo.dev@gmail.com>
2024-04-19 05:42:24 -0700

    finally got the load function to load two notes on a single row.

--- index.js
+++ index.js
@@ -3,23 +3,37 @@ load();
 let notes = [
     {num: 1, note: "Hi this is my first note"},
     {num: 2, note: "Hi this is my second note"},
-    {num: 3, note: "Hi this is my third note"}
+    {num: 3, note: "Hi this is my third note"},
+    {num: 4, note: "Hi this is my fourth note"},
+    {num: 5, note: "Hi this is my fifth note"}
 ]
 
-localStorage.setItem("notes", JSON.stringify(notes))
+//localStorage.setItem("notes", JSON.stringify(notes))
 function load(){
     console.log("loading notes...")
     let notes = JSON.parse(localStorage.getItem("notes"));
     let table = document.querySelector("#noteTable");
     console.log(notes)
+    let row;
     for(let i = 0; i < notes.length; i++){
+        //If i is even, create a new row and a new data cell appended to the row
         if(i % 2 == 0){
-            table.innerHTML += `<td><h1>${notes[i].num}</h1><p>${notes[i].note}</p><button> View Detail</button></td>`
             console.log("creating row...")
+            row = document.createElement("tr");
+            let td = document.createElement("td");
+            td.innerHTML= `<h1>${notes[i].num}</h1><p>${notes[i].note}</p><button> View Detail</button>`
+            row.appendChild(td);
         }
+        //If i is odd, dont create a row. Only append new data cell to existing row
         else{
-            console.log("not creating row...")
-            table.innerHTML += `<td><h1>${notes[i].num}</h1><p>${notes[i].note}</p><button> View Detail</button></td>`
+            let td = document.createElement("td");
+            td.innerHTML= `<h1>${notes[i].num}</h1><p>${notes[i].note}</p><button> View Detail</button>`
+            row.appendChild(td);
+            table.appendChild(row);
+        }
+        //If the length of all notes is odd, create append a row with only that data cell to the main table
+        if(notes.length % 2 != 0){
+            table.appendChild(row);
         }
     }
     
